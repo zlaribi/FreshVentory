e24cd9a0b9692663c8c885b5dca6f6485bbbd5be
Zied Laribi <laribi.zied@gmail.com>
2023-07-13 17:00:13 +0200

    Update the Sonarcloud analyze files.

--- backend/src/main/java/de/iav/backend/repository/FoodRepository.java
+++ backend/src/main/java/de/iav/backend/repository/FoodRepository.java
@@ -1,5 +1,6 @@
 package de.iav.backend.repository;
 
+import de.iav.backend.model.Food;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
