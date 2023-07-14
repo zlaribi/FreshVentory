047c1bc79896ffb663fe3dafd14c31ae1939c714
Zied Laribi <laribi.zied@gmail.com>
2023-07-14 12:00:12 +0200

    Change Application name from HelloApplication to MainPageApplication.

--- frontend/src/main/java/de/iav/frontend/MainPageApplication.java
+++ frontend/src/main/java/de/iav/frontend/MainPageApplication.java
@@ -8,10 +8,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainPageApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainPageApplication.class.getResource("/de/iav/frontend/fxml/hello-view.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("FreshVentory");
--- frontend/src/main/java/de/iav/frontend/MainPageController.java
+++ frontend/src/main/java/de/iav/frontend/MainPageController.java
@@ -3,7 +3,7 @@ package de.iav.frontend;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainPageController {
     @FXML
     private Label welcomeText;
 
