9cd83f2f62ba037ebaf90819fe9e9467bd8dcff4
Zied Laribi <laribi.zied@gmail.com>
2023-07-14 11:39:56 +0200

    Fix .fxml file

--- frontend/src/main/java/de/iav/frontend/HelloApplication.java
+++ frontend/src/main/java/de/iav/frontend/HelloApplication.java
@@ -2,6 +2,7 @@ package de.iav.frontend;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("FreshVentory");
         stage.setScene(scene);
         stage.show();
     }
