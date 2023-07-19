467e1dc72ef7fec3b7d590e2bc32b29663208949
Zied Laribi <laribi.zied@gmail.com>
2023-07-19 16:39:50 +0200

    delete add food page Stage, keep login page only.

--- frontend/src/main/java/de/iav/frontend/FreshVentoryApplication.java
+++ frontend/src/main/java/de/iav/frontend/FreshVentoryApplication.java
@@ -8,24 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class FreshVentoryApplication extends Application {
-    private Stage stage;
-
-   /* @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FreshVentoryApplication.class.getResource("/de/iav/frontend/fxml/login-page.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load(), 360, 780);
-        stage.setTitle("Login page");
-        stage.setScene(scene);
-        stage.show();
-    }*/
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FreshVentoryApplication.class.getResource("/de/iav/frontend/fxml/addfood-page.fxml"));
-
+        FXMLLoader fxmlLoader = new FXMLLoader(FreshVentoryApplication.class.getResource("/de/iav/frontend/fxml/login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 360, 780);
-        stage.setTitle("Add your food");
+        stage.setTitle("Login page");
         stage.setScene(scene);
         stage.show();
     }
