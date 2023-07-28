5961f038e8c4ce8f64b2b9e6b4dfe6d1b2e6f0a2
Gerd Lödige <gerd.loedige@posteo.de>
2023-07-28 11:55:29 +0200

    fix sonar issues

---
+++ frontend/src/main/java/de/iav/frontend/exception/FailedToOpenRuntimeException.java
@@ -0,0 +1,7 @@
+package de.iav.frontend.exception;
+
+public class FailedToOpenRuntimeException extends RuntimeException {
+    public FailedToOpenRuntimeException(String e) {
+
+    }
+}
--- frontend/src/main/java/de/iav/frontend/service/FoodService.java
+++ frontend/src/main/java/de/iav/frontend/service/FoodService.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import de.iav.frontend.exception.FailedToOpenRuntimeException;
 import de.iav.frontend.model.Food;
 import de.iav.frontend.model.FoodWithoutId;
-import de.iav.frontend.security.AuthService;
 import javafx.application.Platform;
 import javafx.scene.control.TableView;
 
@@ -21,6 +21,8 @@ public class FoodService {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final String FOOD_BASE_URL = "http://localhost:8080/api/freshventory"; //http://oxygen.capstone-project.de/api/freshventory
     private final String header_var = "application/json";
+    private final String COOKIE = "Cookie";
+    private final String JSESSION = "\"JSESSIONID=\" + AuthService.getInstance().getSessionId()";
 
     private static FoodService instance;
 
@@ -36,7 +38,7 @@ public class FoodService {
     }
 
     public Food getFoodById(String id) {
-        HttpRequest request = HttpRequest.newBuilder().header("Cookie", "JSESSIONID=" + AuthService.getInstance().getSessionId())
+        HttpRequest request = HttpRequest.newBuilder().header(COOKIE, JSESSION)
                 .GET()
                 .uri(URI.create(FOOD_BASE_URL + "/" + id))
                 .build();
@@ -46,16 +48,16 @@ public class FoodService {
                 .join();
     }
 
-    private Food mapToFood(String json) {
+    private Food mapToFood(String json) throws FailedToOpenRuntimeException {
         try {
             return objectMapper.readValue(json, Food.class);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to open food!", e);
+            throw new FailedToOpenRuntimeException("Failed to open food!");
         }
     }
 
     public List<Food> getFoodList() {
-        HttpRequest request = HttpRequest.newBuilder().header("Cookie", "JSESSIONID=" + AuthService.getInstance().getSessionId())
+        HttpRequest request = HttpRequest.newBuilder().header(COOKIE, JSESSION)
                 .GET()
                 .uri(URI.create(FOOD_BASE_URL + "/food"))
                 .build();
@@ -70,14 +72,14 @@ public class FoodService {
             return objectMapper.readValue(json, new TypeReference<List<Food>>() {
             });
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to open food!", e);
+            throw new FailedToOpenRuntimeException("Failed to open food!");
         }
     }
 
     public Food addFood(FoodWithoutId foodToAdd) {
         try {
             String requestBody = objectMapper.writeValueAsString(foodToAdd);
-            HttpRequest request = HttpRequest.newBuilder().header("Cookie", "JSESSIONID=" + AuthService.getInstance().getSessionId())
+            HttpRequest request = HttpRequest.newBuilder().header(COOKIE, JSESSION)
                     .uri(URI.create(FOOD_BASE_URL + "/food"))
                     .header("Content-Type", header_var)
                     .header("Accept", header_var)
@@ -88,14 +90,14 @@ public class FoodService {
                     .thenApply(this::mapToFood)
                     .join();
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            throw new FailedToOpenRuntimeException("Failed to add food!");
         }
     }
 
     public Food updateFoodById(String id, Food foodToAdd) {
         try {
             String requestBody = objectMapper.writeValueAsString(foodToAdd);
-            HttpRequest request = HttpRequest.newBuilder().header("Cookie", "JSESSIONID=" + AuthService.getInstance().getSessionId())
+            HttpRequest request = HttpRequest.newBuilder().header(COOKIE, JSESSION)
                     .uri(URI.create(FOOD_BASE_URL + "/" + id))
                     .header("Content-Type", header_var)
                     .header("Accept", header_var)
@@ -106,12 +108,12 @@ public class FoodService {
                     .thenApply(this::mapToFood)
                     .join();
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            throw new FailedToOpenRuntimeException("Failed to update food!");
         }
     }
 
     public void deleteFoodById(String idToDelete, TableView<Food> listView) {
-        HttpRequest request = HttpRequest.newBuilder().header("Cookie", "JSESSIONID=" + AuthService.getInstance().getSessionId())
+        HttpRequest request = HttpRequest.newBuilder().header(COOKIE, JSESSION)
                 .uri(URI.create(FOOD_BASE_URL + "/" + idToDelete))
                 .DELETE()
                 .build();
@@ -123,7 +125,7 @@ public class FoodService {
                             listView.refresh();
                             });
                         } else {
-                            throw new RuntimeException("Error while deleting food with ID: " + idToDelete);
+                        throw new FailedToOpenRuntimeException("Error while deleting food with ID: " + idToDelete);
                         }
                     })
                     .join();
