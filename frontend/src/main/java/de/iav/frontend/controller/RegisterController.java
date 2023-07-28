0ad187a27cf07e1f850e7f4a66183a98f23a48b9
Gerd Lödige <gerd.loedige@posteo.de>
2023-07-28 16:07:04 +0200

    add back button from register scene

--- frontend/src/main/java/de/iav/frontend/controller/RegisterController.java
+++ frontend/src/main/java/de/iav/frontend/controller/RegisterController.java
@@ -4,8 +4,10 @@ import de.iav.frontend.FreshVentoryApplication;
 import de.iav.frontend.exception.CustomIOException;
 import de.iav.frontend.security.AppUserRequest;
 import de.iav.frontend.security.AuthService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -24,6 +26,21 @@ public class RegisterController {
     public PasswordField passwordInput;
     @FXML
     public Label errorLabel;
+
+    @FXML
+    protected void onClickBackButton(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(FreshVentoryApplication.class.getResource("/de/iav/frontend/fxml/login-scene.fxml"));
+        Parent root;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new CustomIOException(e.toString());
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     protected void onRegisterClick() {
